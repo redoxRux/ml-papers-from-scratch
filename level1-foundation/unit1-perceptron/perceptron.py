73796501e7b046a566a959355fdbf6e3962c28c1
rux <redoxrux@gmail.com>
2026-05-05 09:10:55 +0000

    update perceptron and train.py

--- level1-foundation/unit1-perceptron/perceptron.py
+++ level1-foundation/unit1-perceptron/perceptron.py
@@ -1,18 +1,18 @@
-import numpy as np 
+import numpy as np
 
 class Perceptron:
-    def __init__(self, input_size, learning_rate=0.1, threshhold=0.5):
+    def __init__(self, input_size, learning_rate=0.1, threshold=0.5):
         self.weights = np.random.randn(input_size) * 0.01
-        self.lr= learning_rate
-        self.threshhold = threshhold
+        self.lr = learning_rate
+        self.threshold = threshold
 
     def predict(self, x):
-        total_sum = np.dot(self.weights * x)
-        return 1 if total_sum >= self.threshhold else 0
+        total_sum = np.dot(self.weights, x)
+        return 1 if total_sum >= self.threshold else 0
 
     def fit(self, X, y, epochs=100):
         for epoch in range(epochs):
             for x_i, y_i in zip(X, y):
-                prediction = predict(x_i)
+                prediction = self.predict(x_i)
                 error = y_i - prediction
                 self.weights += self.lr * error * x_i
