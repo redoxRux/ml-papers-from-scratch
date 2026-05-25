ac467f33edf1552c4ece91193192acbc6b807a4e
rux <redoxrux@gmail.com>
2026-05-25 08:31:24 +0000

    add pooling

--- level1-foundation/unit3-lenet/lenet.py
+++ level1-foundation/unit3-lenet/lenet.py
@@ -14,3 +14,17 @@ def convolution(image_array, filter_array):
             output_array[i][j] = np.sum(patch * filter_array)
     
     return output_array
+
+
+def pooling(feature_map, pool_size):
+    feature_map_height, feature_map_width = feature_map.shape
+    output_height = feature_map_height // pool_size
+    output_width = feature_map_width // pool_size
+    pool_map = np.zeros((output_height,output_width))
+
+    for i in range(output_height):
+        for j in range(output_width):
+            block = feature_map[i* pool_size:i*pool_size + pool_size,j* pool_size:j*pool_size+pool_size]
+            pool_map[i][j] = np.max(block)
+
+    return pool_map
